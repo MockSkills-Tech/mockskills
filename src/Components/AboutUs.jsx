4ef30540f5947e9549a1c674e8d2dac0c0877f22
kundan singh <139676878+kundansinghdev@users.noreply.github.com>
2024-11-04 20:51:52 +0530

    Update AboutUs.jsx

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -14,6 +14,7 @@ const ValueCard = ({ imageSrc, title, description }) => (
     <h3 className="text-2xl font-bold mb-2 text-blue-700">{title}</h3>
     <p className="text-gray-600">{description}</p>
   </div>
+  </div>
 );
 
 // Reusable card component for team members
