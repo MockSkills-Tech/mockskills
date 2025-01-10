6a6addc3b47ff5a9ce0765d3ba33aeb02d23d0bf
Mithalesh Kumar(HSNTech Solutions) <mikumar@americana-food.com>
2025-01-11 00:31:53 +0530

    fixex

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import CourseDetail from "./Components/Courses/CourseDetails/CourseDetail";
 import SingleCourseDetail from "./Components/Courses/CourseDetails/SingleCourseDetail";
 import CollabZone from "./Components/CollabZone/CollabZone";
 import Partners from "./Components/Partners/Partners";
-import BattleGround from "./Components/BattleGround/Battleground"; 
+import BattleGround from "./Components/BattleGround/BattleGround"; 
 import GlobalProtection from "./GlobalProtection"; // Apply GlobalProtection globally
 import ContentBody from "./Components/Courses/ContentDetails/ContentBody";
 import FindGenZ from "./Components/CollabZone/CollabFeatures/FindGenZ";
